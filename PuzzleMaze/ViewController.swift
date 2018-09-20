c82aebad6539dba474cea86e10c86538919eafaa
Viktor Lott <viktorlott@viktors-macbook-air.local>
2018-09-20 11:05:20 +0200

    Initial Commit

---
+++ PuzzleMaze/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PuzzleMaze
+//
+//  Created by Viktor Lott on 9/20/18.
+//  Copyright © 2018 Viktor Lott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
